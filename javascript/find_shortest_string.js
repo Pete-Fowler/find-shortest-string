5df8e942441390f95529fe26055c190bd1bab312
Pete Fowler <fowler.pete@gmail.com>
2022-08-19 15:24:33 -0600

    Add solution

--- javascript/find_shortest_string.js
+++ javascript/find_shortest_string.js
@@ -1,9 +1,30 @@
+/*
+PROBLEM: Given an array of strings, return the shortest string. If two are 
+equal, return the one that comes first.
+
+APPROACH: Input is array of strings. Output is a string. Pseudocode:
+loop through the array
+  for the string at each index
+    convert it to an array
+    get the length
+    if the length is shorter than the stored shortest length
+      store current value as the shortest
+    continue loop
+  return shortest
+*/
+
+
 function findShortestString(arr) {
-  // type your code here
+  
+  const string = arr.reduce((last, current) => {
+    return current.split('').length < last.split('').length ? current : last;
+  });
+  
+  return string;
 }
 
 if (require.main === module) {
-  // add your own tests in here
+
   console.log("Expecting: 'a'");
   console.log("=>", findShortestString(['aaa', 'a', 'bb', 'ccc']));
 
@@ -17,10 +38,8 @@ if (require.main === module) {
   console.log("Expecting: 'lily'");
   console.log("=>", findShortestString(['flower', 'juniper', 'lily', 'dadelion']));
 
-  // BENCHMARK HERE
 }
 
 module.exports = findShortestString;
 
-// Please add your pseudocode to this file
-// And a written explanation of your solution
+
